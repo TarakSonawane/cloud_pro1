4cf63813546bf0791cf735631d434bfd39df584c
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2025-05-06 14:07:53 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = "backend-cloud-project-d7bjbkd3fhd3ajbc.canadacentral-01.azurewebsites.net";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
