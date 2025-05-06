fdcb37d09668a8444305d98ba84774ebd39cc7d7
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2025-05-06 14:49:12 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "backend-cloud-project-d7bjbkd3fhd3ajbc.canadacentral-01.azurewebsites.net/api",
+  baseURL: "https://backend-cloud-project-d7bjbkd3fhd3ajbc.canadacentral-01.azurewebsites.net/api",
   withCredentials: true,
 });
